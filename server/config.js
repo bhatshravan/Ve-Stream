ec1a180644e1048641a229bf21e1cfee36927189
shravan <shrvnbhat0@gmail.com>
2018-12-16 21:07:58 +0530

    Added rendering engine and mock views

--- server/config.js
+++ server/config.js
@@ -1,7 +1,13 @@
 const db_url = require('./private/db.js');
 
 var configs = {
+  BUILD: 0,
+  VERSION: 1,
+  PORT: 8080,
+  ip_Address: 'http://127.0.0.1',
+
   mongodb_url : db_url,
+
 
   //Get by using config.testing()
   testing2 : function() {
--- server/index.js
+++ server/index.js
@@ -1,4 +1,33 @@
+var express = require('express');
+const mongoose = require('mongoose');
 const config = require('./config.js');
+const path = require('path');
 
-console.log("Started server at:"+config.mongodb_url);
-console.log(config.testing2());
+//Connect to mongodb
+const MongoDb = process.env.MONGODB_URI || config.mongodb_url;
+mongoose.connect(MongoDb, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error',console.error.bind(console, 'MongoDb connection error'));
+
+//Connect to express
+var app = express();
+
+//Set up a render engine which is ejs
+app.set('view engine', 'ejs');
+app.set('views', 'server/views/ejs');
+
+
+//Home page
+app.all('/', (req,res) => {
+  res.sendFile(path.join(__dirname + '/client/index.html'));
+});
+
+//Home page
+app.all('/hello', (req,res) => {
+  res.render('index');
+});
+
+app.listen(config.PORT, function(){
+  console.log("Started server at: "+config.ip_Address+":"+config.PORT);
+});
